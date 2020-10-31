2b92c0b05d5b9cff75ae0089ae53070d80f8035e
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-10-31 16:32:07 +0300

    Scripted auto-commit on change (2020-10-31 16:32:07) by gitwatch.sh

--- lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/AirportsJoinMapper.java
+++ lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/AirportsJoinMapper.java
@@ -14,7 +14,7 @@ public class AirportsJoinMapper extends Mapper<LongWritable, Text, TextPair, Tex
     //т.к. отсчет с нуля, то индексы на единицу меньше, чем в самом csv файле
     private static final int DEST_AIRPORT_ID_COLUMN_NUMBER = 0; // DEST_AIRPORT_ID — Идентификатор аэропорта
 
-    private static final int FILE_NUMBER = 0; // номер входного файла (для этапа Reduce)
+    private static final Integer FILE_NUMBER = 0; // номер входного файла (для этапа Reduce)
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
