599ce62ab732070a5224413595e2d375e294a033
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-10-31 12:22:51 +0300

    Scripted auto-commit on change (2020-10-31 12:22:50) by gitwatch.sh

--- lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/AirportsJoinMapper.java
+++ lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/AirportsJoinMapper.java
@@ -9,11 +9,21 @@ import org.apache.hadoop.io.Text;
 import java.io.IOException;
 
 public class AirportsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+
+    //т.к. отсчет с нуля, то индексы на единицу меньше, чем в самом csv файле
+    private static final int DEST_AIRPORT_ID_COLUMN_NUMBER = 0; // DEST_AIRPORT_ID — Идентификатор аэропорта
+
+    private static final int AIRPORT_DESCRIPTION_COLUMN_NUMBER = 1; // DEST_AIRPORT_ID — Идентификатор аэропорта
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (!key.equals(new LongWritable(0))) { // Пропускаем первую строку csv файла (наименования столбцов)
-            SystemInfo system = new SystemInfo(value);
-            context.write(new TextPair(system.getSystemCode().toString(), "0"), new Text(system.toString()));  // key, value
+            String[] columns = value.toString().split(",");
+
+            Integer destAirportId = Integer.parseInt(columns[DEST_AIRPORT_ID_COLUMN_NUMBER]);
+            String airportDescription = columns[AIRPORT_DESCRIPTION_COLUMN_NUMBER];
+
+            context.write(new TextPair(destAirportId, "0"), new Text(airportDescription));  // key, value
         }
     }
 }
