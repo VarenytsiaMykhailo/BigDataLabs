7740d33388ed52e40495736005aa7e2e72d28d04
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-10-31 08:41:25 +0300

    Scripted auto-commit on change (2020-10-31 08:41:25) by gitwatch.sh

--- lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/ReduceSideJoinRunner.java
+++ lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/ReduceSideJoinRunner.java
@@ -1,4 +1,35 @@
 package com.github.varenytsiamykhailo.BigDataLabs.lab2;
 
-public class ReduceSideJoin {
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class ReduceSideJoinRunner {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: ReduceSideJoinRunner <input path A> <input path B> <output path>");
+            System.exit(-1);
+        }
+        Job job = Job.getInstance();
+        job.setJarByClass(ReduceSideJoinRunner.class);
+        job.setJobName("ReduceSideJoinRunner");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(TextPair.FirstPartitioner.class);
+        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+        job.setReducerClass(JoinReducer.class);
+        job.setMapOutputKeyClass(TextPair.class);
+        
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 }
