7c9f4579f144478c7ffb0afd234efa72f4fac080
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-10-31 11:24:23 +0300

    Scripted auto-commit on change (2020-10-31 11:24:23) by gitwatch.sh

--- lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/Main.java
+++ lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/Main.java
@@ -1,24 +1,30 @@
 package com.github.varenytsiamykhailo.BigDataLabs.lab2;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        BufferedReader csvReader = new BufferedReader(new FileReader("technicalTask/flights.csv");
-        String row = "";
-        while (true) {
-            try {
-                if (!((row = csvReader.readLine()) != null))
-                    break;
-                String[] data = row.split(",");
-                // do something with the data
-                csvReader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        BufferedReader csvReader = null;
+        try {
+            csvReader = new BufferedReader(new FileReader("technicalTask/airports.csv"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
+        String row = "";
+
+        try {
+            while ((row = csvReader.readLine()) != null) {
+                System.out.println(row);
+                //String[] data = row.split(",");
+            }
+            csvReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
