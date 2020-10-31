a1158dd026d961deed7e90bd6ea6e1442cafe432
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-10-31 12:18:16 +0300

    Scripted auto-commit on change (2020-10-31 12:18:16) by gitwatch.sh

--- lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/AirportsJoinMapper.java
+++ lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/AirportsJoinMapper.java
@@ -1,4 +1,4 @@
-/*
+
 package com.github.varenytsiamykhailo.BigDataLabs.lab2;
 
 import org.apache.hadoop.mapreduce.Mapper;
@@ -11,7 +11,9 @@ import java.io.IOException;
 public class AirportsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        SystemInfo system = new SystemInfo(value);
-        context.write(new TextPair(system.getSystemCode().toString(), "0"), new Text(system.toString()));  // key, value
+        if (!key.equals(new LongWritable(0))) { // Пропускаем первую строку csv файла (наименования столбцов)
+            SystemInfo system = new SystemInfo(value);
+            context.write(new TextPair(system.getSystemCode().toString(), "0"), new Text(system.toString()));  // key, value
+        }
     }
-}*/
+}
--- lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/FlightsJoinMapper.java
+++ lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/FlightsJoinMapper.java
@@ -9,18 +9,21 @@ import java.io.IOException;
 
 public class FlightsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
 
-    private static final int DEST_AIRPORT_ID_COLUMN_NUMBER = 14; // т.к. отсчет с нуля (в csv файле - 15ый столбец)
+    //т.к. отсчет с нуля, то индексы на единицу меньше, чем в самом csv файле
+    private static final int DEST_AIRPORT_ID_COLUMN_NUMBER = 14; // DEST_AIRPORT_ID — Идентификатор аэропорта
 
-    private static final int ARR_DELAY_NEW_COLUMN_NUMBER = 18
+    private static final int ARR_DELAY_NEW_COLUMN_NUMBER = 18; // ARR_DELAY_NEW - разница в минутах между расчетным временем приземления и реальным (>=0)
+
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (!key.equals(new LongWritable(0))) { // Пропускаем первую строку csv файла (наименования столбцов)
             String[] columns = value.toString().split(",");
-            Integer destAirportId = Integer.parseInt(columns[14]);
-            ServiceCall call = new ServiceCall(value);
 
-            context.write(new TextPair(call.getSystemA().toString(), "1"), new Text(call.toString())); // key, value
+            Integer destAirportId = Integer.parseInt(columns[DEST_AIRPORT_ID_COLUMN_NUMBER]);
+            String delay = columns[ARR_DELAY_NEW_COLUMN_NUMBER];
+
+            context.write(new TextPair(destAirportId, "1"), new Text(delay)); // key, value
         }
     }
 }
