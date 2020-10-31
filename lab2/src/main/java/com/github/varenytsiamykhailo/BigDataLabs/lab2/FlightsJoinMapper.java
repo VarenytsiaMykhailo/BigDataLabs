fe79590fb913231c69d67501276dc9de3c61e83a
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-10-31 12:11:11 +0300

    Scripted auto-commit on change (2020-10-31 12:11:11) by gitwatch.sh

--- lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/FlightsJoinMapper.java
+++ lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/FlightsJoinMapper.java
@@ -8,11 +8,15 @@ import org.apache.hadoop.io.Text;
 import java.io.IOException;
 
 public class FlightsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+
+    private static final int DEST_AIRPORT_ID_COLUMN_NUMBER = 14; // т.к. отсчет с нуля (в csv файле - 15ый столбец)
+
+    private static final int
+
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (!key.equals(new LongWritable(0))) { // Пропускаем первую строку csv файла (наименования столбцов)
             String[] columns = value.toString().split(",");
-
             Integer destAirportId = Integer.parseInt(columns[14]);
             ServiceCall call = new ServiceCall(value);
 
