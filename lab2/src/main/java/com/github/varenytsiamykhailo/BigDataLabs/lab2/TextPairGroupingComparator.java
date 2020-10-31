564066461bea30d4ce64d44709466960c93bda66
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-10-31 12:57:47 +0300

    Scripted auto-commit on change (2020-10-31 12:57:47) by gitwatch.sh

--- lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/TextPairGroupingComparator.java
+++ lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/TextPairGroupingComparator.java
@@ -1,4 +1,15 @@
 package com.github.varenytsiamykhailo.BigDataLabs.lab2;
 
-public class TextPairGroupingComparator {
+import org.apache.hadoop.io.RawComparator;
+
+public class TextPairGroupingComparator implements RawComparator {
+    @Override
+    public int compare(byte[] bytes, int i, int i1, byte[] bytes1, int i2, int i3) {
+        return 0;
+    }
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        return 0;
+    }
 }
