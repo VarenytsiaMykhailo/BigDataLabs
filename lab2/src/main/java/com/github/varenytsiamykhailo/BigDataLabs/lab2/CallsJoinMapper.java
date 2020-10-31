de0b49f23c54df6a8dcb41668b31e35e9d17e144
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-10-31 08:29:09 +0300

    Scripted auto-commit on change (2020-10-31 08:29:08) by gitwatch.sh

--- lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/CallsJoinMapper.java
+++ lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/CallsJoinMapper.java
@@ -12,5 +12,7 @@ public class CallsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> 
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         ServiceCall call = new ServiceCall(value);
         context.write(new TextPair(call.getSystemA().toString(), "1"), new Text(call.toString())); // key, value
+
+    asd
     }
 }
