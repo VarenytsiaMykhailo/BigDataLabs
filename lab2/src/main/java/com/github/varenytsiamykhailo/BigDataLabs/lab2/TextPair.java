931c4df7d3714fac8e6b0be9a377ff35a7e21c12
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-10-31 12:24:57 +0300

    Scripted auto-commit on change (2020-10-31 12:24:57) by gitwatch.sh

--- lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/TextPair.java
+++ lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/TextPair.java
@@ -1,4 +1,27 @@
 package com.github.varenytsiamykhailo.BigDataLabs.lab2;
 
-public class TextPair {
+import org.apache.hadoop.io.WritableComparable;
+
+import javax.xml.soap.Text;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class TextPair implements WritableComparable<TextPair> {
+
+
+    @Override
+    public int compareTo(TextPair o) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
