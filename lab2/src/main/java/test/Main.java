2d78f991fc7348c1219fb3d574a0e35921afc6eb
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-10-31 11:42:09 +0300

    Scripted auto-commit on change (2020-10-31 11:42:09) by gitwatch.sh

--- lab2/src/main/java/test/Main.java
+++ lab2/src/main/java/test/Main.java
@@ -21,8 +21,8 @@ public class Main {
                 // System.out.println(row);
                 String[] data = row.split(",");
                 Integer destAirportId = Integer.parseInt(data[14]);
-                System.out.println(destAirportId);
-                //System.out.println(Arrays.toString(data));
+                //System.out.println(destAirportId);
+                System.out.println(Arrays.toString(data));
             }
             csvReader.close();
         } catch (IOException e) {
