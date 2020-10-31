ed6cf124fcb61f686e7ed9ebd5f0d1aa390c8bf6
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-10-31 07:38:24 +0300

    Scripted auto-commit on change (2020-10-31 07:38:23) by gitwatch.sh

---
+++ lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/JoinReducer.java
@@ -0,0 +1,23 @@
+package com.github.varenytsiamykhailo.BigDataLabs.lab2;
+
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
+    @Override
+    protected void reduce(TextPair key, Iterable<Text> values, Context context) throws
+            IOException, InterruptedException {
+        Iterator<Text> iter = values.iterator();
+        Text systemInfo = new Text(iter.next());
+        while (iter.hasNext()) {
+            Text call = iter.next();
+            Text outValue = new Text(call.toString() + "\t" + systemInfo.toString());
+            context.write(key.getFirst(), outValue);
+        }
+    }
+}
