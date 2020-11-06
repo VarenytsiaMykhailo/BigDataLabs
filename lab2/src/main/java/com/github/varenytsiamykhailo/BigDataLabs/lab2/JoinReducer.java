5ff56191ea68ebcabd799e7b01365df7818bdb13
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-11-07 02:21:27 +0300

    Scripted auto-commit on change (2020-11-07 02:21:26) by gitwatch.sh

--- lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/JoinReducer.java
+++ lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/JoinReducer.java
@@ -17,11 +17,37 @@ public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
         Iterator<Text> iter = values.iterator();
         String airportDescription = iter.next().toString();
 
-        StatisticsCalculator statisticsCalculator = new StatisticsCalculator(values);
+       /* StatisticsCalculator statisticsCalculator = new StatisticsCalculator(values);
 
         String result = "Airport name = " + airportDescription + "; Min delay = " + statisticsCalculator.getMinDelay() +
                 "; Max delay = " + statisticsCalculator.getMaxDelay() + "; Average delay =  " + statisticsCalculator.getAverageDelay() + ";";
         // Text outValue = new Text("+++++++++ call = " + call.toString() + "------------ airportDescription = " + airportDescription.toString() + "/++++++++++");
+
+*/
+        double minDelay = Double.MAX_VALUE;
+
+        double maxDelay = 0.0;
+
+        double sumOfDelays = 0.0;
+
+        int countOfDelays = 0;
+
+        double averageDelay = 0.0;
+
+        while (iter.hasNext()) {
+            double delay = Double.parseDouble(iter.next().toString());
+            if (delay < minDelay) {
+                minDelay = delay;
+            }
+            if (delay > maxDelay) {
+                maxDelay = delay;
+            }
+            sumOfDelays += delay;
+            countOfDelays++;
+        }
+        averageDelay = sumOfDelays / (double)countOfDelays;
+
+
         context.write(new Text("AirportId = " + key.getDestAirportId().toString() + "; "), new Text(result));
     }
 
