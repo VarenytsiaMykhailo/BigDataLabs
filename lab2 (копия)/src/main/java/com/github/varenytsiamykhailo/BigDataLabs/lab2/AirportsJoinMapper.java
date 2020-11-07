71fd0f4de127604db311bffbc47855c00a5b1cb9
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-11-07 04:35:39 +0300

    Scripted auto-commit on change (2020-11-07 04:35:38) by gitwatch.sh

---
+++ lab2 (копия)/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/AirportsJoinMapper.java
@@ -0,0 +1,52 @@
+
+package com.github.varenytsiamykhailo.BigDataLabs.lab2;
+
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class AirportsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+
+    private static final Integer FILE_NUMBER = 0; // номер входного файла (для этапа Reduce)
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        if (!key.equals(new LongWritable(0))) { // Пропускаем первую строку csv файла (наименования столбцов)
+
+            AirportsParser airportsParser = new AirportsParser(value);
+
+            Integer destAirportId = airportsParser.parseDestAirportId();
+            String airportDescription = airportsParser.parseAirportDescription();
+
+            context.write(new TextPair(destAirportId, FILE_NUMBER), new Text(airportDescription)); // key, value
+        }
+    }
+}
+
+class AirportsParser {
+
+    //т.к. отсчет с нуля, то индексы на единицу меньше, чем в самом csv файле
+    private static final int DEST_AIRPORT_ID_COLUMN_NUMBER = 0; // DEST_AIRPORT_ID — Идентификатор аэропорта
+
+    private String[] columns;
+
+    public AirportsParser(Text value) {
+        this.columns = value.toString().split(",");
+    }
+
+    public Integer parseDestAirportId() {
+        return Integer.parseInt(columns[DEST_AIRPORT_ID_COLUMN_NUMBER].replaceAll("\"", ""));
+    }
+
+    public String parseAirportDescription() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < columns.length; i++) {
+            sb.append(columns[i]);
+        }
+        return sb.toString();
+    }
+}
---
+++ lab2 (копия)/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/FlightsJoinMapper.java
@@ -0,0 +1,51 @@
+package com.github.varenytsiamykhailo.BigDataLabs.lab2;
+
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class FlightsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+
+    private static final Integer FILE_NUMBER = 1; // номер входного файла (для этапа Reduce)
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        if (!key.equals(new LongWritable(0))) { // Пропускаем первую строку csv файла (наименования столбцов)
+
+            FlightsParser flightsParser = new FlightsParser(value);
+
+            Integer destAirportId = flightsParser.parseDestAirportId();
+            String delay = flightsParser.parseDelay();
+
+            if (!(delay.isEmpty())) {
+                context.write(new TextPair(destAirportId, FILE_NUMBER), new Text(delay));
+            }
+        }
+    }
+}
+
+class FlightsParser {
+
+    //т.к. отсчет с нуля, то индексы на единицу меньше, чем в самом csv файле
+    private static final int DEST_AIRPORT_ID_COLUMN_NUMBER = 14; // DEST_AIRPORT_ID — Идентификатор аэропорта
+
+    private static final int ARR_DELAY_NEW_COLUMN_NUMBER = 18; // ARR_DELAY_NEW - разница в минутах между расчетным временем приземления и реальным (>=0)
+
+    private String[] columns;
+
+    public FlightsParser(Text value) {
+        this.columns = value.toString().replaceAll(" ","").split(",");
+    }
+
+    public Integer parseDestAirportId() {
+        return Integer.parseInt(columns[DEST_AIRPORT_ID_COLUMN_NUMBER].replaceAll("\"",""));
+    }
+
+    public String parseDelay() {
+        return columns[ARR_DELAY_NEW_COLUMN_NUMBER];
+    }
+}
