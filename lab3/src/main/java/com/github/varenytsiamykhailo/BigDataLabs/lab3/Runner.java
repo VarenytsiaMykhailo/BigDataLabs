56a38e0049a789e46dcfce8c797be0701551c130
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-11-07 05:27:01 +0300

    Scripted auto-commit on change (2020-11-07 05:27:01) by gitwatch.sh

--- lab3/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab3/Runner.java
+++ lab3/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab3/Runner.java
@@ -1,5 +1,20 @@
 package com.github.varenytsiamykhailo.BigDataLabs.lab3;
 
-public class Runner {
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 
+import java.util.Arrays;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> lines = sc.parallelize(Arrays.asList("pandas", "i like pandas"));
+        System.out.println("123");
+        lines.collect();
+
+    }
 }
