f558e2de3da958d907c45ab742ebf098f3e9cb40
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-11-07 03:42:52 +0300

    Scripted auto-commit on change (2020-11-07 03:42:51) by gitwatch.sh

--- lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/FlightsJoinMapper.java
+++ lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/FlightsJoinMapper.java
@@ -10,24 +10,42 @@ import java.util.Arrays;
 
 public class FlightsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
 
-    //т.к. отсчет с нуля, то индексы на единицу меньше, чем в самом csv файле
-    private static final int DEST_AIRPORT_ID_COLUMN_NUMBER = 14; // DEST_AIRPORT_ID — Идентификатор аэропорта
-
-    private static final int ARR_DELAY_NEW_COLUMN_NUMBER = 18; // ARR_DELAY_NEW - разница в минутах между расчетным временем приземления и реальным (>=0)
-
     private static final Integer FILE_NUMBER = 1; // номер входного файла (для этапа Reduce)
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (!key.equals(new LongWritable(0))) { // Пропускаем первую строку csv файла (наименования столбцов)
-            String[] columns = value.toString().replaceAll(" ","").split(",");
 
-            Integer destAirportId = Integer.parseInt(columns[DEST_AIRPORT_ID_COLUMN_NUMBER].replaceAll("\"",""));
-            String delay = columns[ARR_DELAY_NEW_COLUMN_NUMBER];
+            FlightsParser flightsParser = new FlightsParser(value);
+
+            Integer destAirportId = flightsParser.parseDestAirportId();
+
 
             if (!(delay.isEmpty())) {
                 context.write(new TextPair(destAirportId, FILE_NUMBER), new Text(delay)); // key, value
             }
         }
+    }
+}
+
+class FlightsParser {
+
+    //т.к. отсчет с нуля, то индексы на единицу меньше, чем в самом csv файле
+    private static final int DEST_AIRPORT_ID_COLUMN_NUMBER = 14; // DEST_AIRPORT_ID — Идентификатор аэропорта
+
+    private static final int ARR_DELAY_NEW_COLUMN_NUMBER = 18; // ARR_DELAY_NEW - разница в минутах между расчетным временем приземления и реальным (>=0)
+
+    private String[] columns;
+
+    public FlightsParser(Text value) {
+        String[] columns = value.toString().replaceAll(" ","").split(",");
+    }
+
+    public Integer parseDestAirportId() {
+        return Integer.parseInt(columns[DEST_AIRPORT_ID_COLUMN_NUMBER].replaceAll("\"",""));
+    }
+
+    public String parseDelay() {
+        return columns[ARR_DELAY_NEW_COLUMN_NUMBER];
     }
 }
