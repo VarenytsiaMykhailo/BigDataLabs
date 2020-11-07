83a0d0a8ab1daac0b9cd43a829ecdfc4f948fb7c
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-11-07 03:46:38 +0300

    Scripted auto-commit on change (2020-11-07 03:46:38) by gitwatch.sh

--- lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/JoinReducer.java
+++ lab2/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab2/JoinReducer.java
@@ -1,8 +1,6 @@
 package com.github.varenytsiamykhailo.BigDataLabs.lab2;
 
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 
 import java.io.IOException;
