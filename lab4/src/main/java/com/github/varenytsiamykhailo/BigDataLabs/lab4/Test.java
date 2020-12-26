7ea27432cb0fb2b46d11ac2a068d3ee7eb211bfc
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-12-26 20:28:40 +0300

    Scripted auto-commit on change (2020-12-26 20:28:39) by gitwatch.sh

--- lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/Test.java
+++ lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/Test.java
@@ -1,4 +1,8 @@
 package com.github.varenytsiamykhailo.BigDataLabs.lab4;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Test {
+
 }
