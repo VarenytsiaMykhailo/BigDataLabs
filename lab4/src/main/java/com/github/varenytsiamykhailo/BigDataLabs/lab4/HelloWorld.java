9f7f5c07e491b1a12f59bb3dc1294c3cf57c8286
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-12-26 15:43:17 +0300

    Scripted auto-commit on change (2020-12-26 15:43:17) by gitwatch.sh

--- lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/HelloWorld.java
+++ lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/HelloWorld.java
@@ -1,6 +1,6 @@
 package com.github.varenytsiamykhailo.BigDataLabs.lab4;
 
-import akka.actor.typed.java
+import akka.actor.
 
 public class HelloWorld extends AbstractBehavior<HelloWorld.Command> {
 
