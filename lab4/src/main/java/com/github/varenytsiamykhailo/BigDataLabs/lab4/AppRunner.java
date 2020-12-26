06270a96abbca5186fa4684990edc3f891d9ddd7
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-12-26 19:38:19 +0300

    Scripted auto-commit on change (2020-12-26 19:38:19) by gitwatch.sh

--- lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/AppRunner.java
+++ lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/AppRunner.java
@@ -1,7 +1,11 @@
 package com.github.varenytsiamykhailo.BigDataLabs.lab4;
 
 public class AppRunner {
+
     public static void main(String[] args) {
 
+        HttpServer httpServer = new HttpServer();
+
+        httpServer.run();
     }
 }
--- lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/HttpServer.java
+++ lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/HttpServer.java
@@ -1,4 +1,38 @@
 package com.github.varenytsiamykhailo.BigDataLabs.lab4;
 
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+
+import java.util.concurrent.CompletionStage;
+
 public class HttpServer {
+
+    public void run() {
+
+        ActorSystem system = ActorSystem.create("s");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+
+        MainHttp instance = new MainHttp(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute(system).flow(system, materializer);
+
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
+    }
+
 }
