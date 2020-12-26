70d2974d045ff5aa66639dce49a1ca74df5e252d
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-12-26 20:16:34 +0300

    Scripted auto-commit on change (2020-12-26 20:16:33) by gitwatch.sh

--- lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/TestResult.java
+++ lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/TestResult.java
@@ -3,9 +3,22 @@ package com.github.varenytsiamykhailo.BigDataLabs.lab4;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.ArrayList;
+
 public class TestResult {
 
-    public TestResult(@JsonProperty("testName") String testName, @JsonProperty("expectedResult") String expectedResult, @JsonProperty("params") String params) {
+    private String testName;
 
+    private String expectedResult;
+
+    private ArrayList<Integer> params;
+
+    private boolean isSuccessfulTest;
+
+    public TestResult(@JsonProperty("testName") String testName, @JsonProperty("expectedResult") String expectedResult, @JsonProperty("params") isSuccessfulTest params, boolean isSuccessfulTest) {
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.params = params;
+        this.isSuccessfulTest = isSuccessfulTest;
     }
 }
