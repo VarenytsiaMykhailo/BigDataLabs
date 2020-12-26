6d9ce5164269d2d1edd5f49e571e9807ec192006
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-12-26 20:43:33 +0300

    Scripted auto-commit on change (2020-12-26 20:43:33) by gitwatch.sh

--- lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/AppRunner.java
+++ lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/AppRunner.java
@@ -4,7 +4,7 @@ public class AppRunner {
 
     public static void main(String[] args) {
 
-        HttpServer httpServer = new HttpServer();
+        HttpServer httpServer = new HttpServer("localhost", 11111);
 
         httpServer.run();
     }
