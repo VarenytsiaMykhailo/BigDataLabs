15fe5926798798a2f29bbe31bed43af274f509f9
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-12-28 00:30:07 +0300

    Scripted auto-commit on change (2020-12-28 00:30:06) by gitwatch.sh

--- lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/TestExecutionActor.java
+++ lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/TestExecutionActor.java
@@ -2,7 +2,24 @@ package com.github.varenytsiamykhailo.BigDataLabs.lab4;
 
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.japi.pf.ReceiveBuilder;
 
 public class TestExecutionActor extends AbstractActor {
+
+    private static String STORE_ACTOR_NAME = "StoreActor";
+
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create().match(ReceivedMessageByPOST.class, message -> {
+            for (TestResult testResult : message.getTests()) {
+                getContext().actorSelection("/user/" + TEST_EXECUTION_ACTOR_NAME).tell( // Отправляем тест на тестирование в TestExecutionActor
+                        new TestForTestExecutionActor(message.getPackageId(), message.getJsScript(), message.getFunctionName(), testResult),
+                        ActorRef.noSender()
+                );
+            }
+
+        }).build();
+    }
 
 }
