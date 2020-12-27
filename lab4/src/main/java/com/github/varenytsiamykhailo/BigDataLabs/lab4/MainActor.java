b2d8a0ba0d2e9bbd9d2c3f900b35f71c94a07511
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-12-28 00:03:02 +0300

    Scripted auto-commit on change (2020-12-28 00:03:02) by gitwatch.sh

--- lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/MainActor.java
+++ lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/MainActor.java
@@ -1,6 +1,8 @@
 package com.github.varenytsiamykhailo.BigDataLabs.lab4;
 
-public class MainActor extends AbstractActor  {
+import akka.actor.AbstractActor;
+
+public class MainActor extends AbstractActor {
 
     private String TEST_EXECUTION_ACTOR_NAME = "TestExecutionActor";
 
