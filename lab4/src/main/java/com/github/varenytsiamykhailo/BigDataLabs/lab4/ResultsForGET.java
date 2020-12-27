9db576f3144f93b59af15ae89f33d7b5f0d4c7fa
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-12-28 02:34:08 +0300

    Scripted auto-commit on change (2020-12-28 02:34:07) by gitwatch.sh

--- lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/ResultsForGET.java
+++ lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/ResultsForGET.java
@@ -6,17 +6,17 @@ import java.util.List;
 
 public class ResultsForGET {
     private static final String PACKAGE_ID = "packageId";
-    private static final String TESTS = "tests";
+    private static final String TEST_RESULTS = "testResults";
 
     @JsonProperty(PACKAGE_ID)
     private Integer packageId;
 
-    @JsonProperty(TESTS)
-    private List<TestResult> tests;
+    @JsonProperty(TEST_RESULTS)
+    private List<TestResult> testResults;
 
-    public ResultsForGET(@JsonProperty(PACKAGE_ID) Integer packageId, @JsonProperty(TESTS) List<TestResult> tests) {
+    public ResultsForGET(@JsonProperty(PACKAGE_ID) Integer packageId, @JsonProperty(TEST_RESULTS) List<TestResult> testResults) {
         this.packageId = packageId;
-        this.tests = tests;
+        this.testResults = testResults;
     }
 
     public Integer getPackageId() {
