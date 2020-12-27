e72b3f730a6b82c3a393876c3955234c2ca5392f
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-12-27 23:42:11 +0300

    Scripted auto-commit on change (2020-12-27 23:42:11) by gitwatch.sh

--- lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/PackageIdTests.java
+++ lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/PackageIdTests.java
@@ -1,4 +1,36 @@
 package com.github.varenytsiamykhailo.BigDataLabs.lab4;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class PackageIdTests {
+
+    private final String PACKAGE_ID = "packageId";
+    private final String JS_SCRIPT = "jsScript";
+    private final String FUNCTION_NAME = "functionName";
+    private final String TEST_RESULTS = "testResults";
+
+    @JsonProperty(PACKAGE_ID)
+    private Integer packageId;
+
+    @JsonProperty(JS_SCRIPT)
+    private String jsScript;
+
+    @JsonProperty(FUNCTION_NAME)
+    private String functionName;
+
+    @JsonProperty(TEST_RESULT)
+    private TestResult testResult;
+
+    @JsonCreator
+    public Test(@JsonProperty(PACKAGE_ID) Integer packageId,
+                @JsonProperty(JS_SCRIPT) String jsScript,
+                @JsonProperty(FUNCTION_NAME) String functionName,
+                @JsonProperty(TEST_RESULT) TestResult testResult) {
+        this.packageId = packageId;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.testResult = testResult;
+    }
+
 }
