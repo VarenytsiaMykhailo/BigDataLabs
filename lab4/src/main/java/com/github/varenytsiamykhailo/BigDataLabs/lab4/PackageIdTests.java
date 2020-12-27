c1de1c498199dfa4de22b1027905773f528f53c1
VarenytsiaMykhailo <l2fonat@bk.ru>
2020-12-27 23:46:28 +0300

    Scripted auto-commit on change (2020-12-27 23:46:27) by gitwatch.sh

--- lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/PackageIdTests.java
+++ lab4/src/main/java/com/github/varenytsiamykhailo/BigDataLabs/lab4/PackageIdTests.java
@@ -28,7 +28,7 @@ public class PackageIdTests {
     public PackageIdTests(@JsonProperty(PACKAGE_ID) Integer packageId,
                 @JsonProperty(JS_SCRIPT) String jsScript,
                 @JsonProperty(FUNCTION_NAME) String functionName,
-                @JsonProperty(TESTS) TestResult testResult) {
+                @JsonProperty(TESTS) ArrayList<TestResult> tests) {
         this.packageId = packageId;
         this.jsScript = jsScript;
         this.functionName = functionName;
